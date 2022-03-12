c7d3dd3a5f4d0db4a279415ba48cc7ec26174ea8
Kasey <kasey.murdoch@yahoo.com>
2022-03-12 12:13:05 -0800

    Rough ability to push objects into array

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
+const button = document.querySelector(`button`)
+// const titleInput = document.querySelector(`#title`)
+// const authorInput = document.querySelector(`#author`)
+// const pagesInput = document.querySelector(`#pages`)
+// const readInput = document.querySelector(`#read`)
+
+document.addEventListener(`DOMContentLoaded`, () => {
+    button.addEventListener(`click`, addBookToLibrary)
+})
+
 let myLibrary = []
 
 function Book(title, author, pages, read) {
@@ -5,14 +15,15 @@ function Book(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        return title, author, pages, read
-    }
-    console.log(Book.info())
+    // this.info = function() {
+    //     return title, author, pages, read
+    // }
 }
 
 function addBookToLibrary() {
-    
+    let newBook = new Book(document.querySelector(`#title`).value, document.querySelector(`#author`).value, document.querySelector(`#pages`).value, document.querySelector(`#read`).value)
+    myLibrary.push(newBook)
+    console.log(newBook)
+    console.log(myLibrary)
+    // document.querySelector(`div`).textContent = myLibrary
 }
-
-addBookToLibrary()
