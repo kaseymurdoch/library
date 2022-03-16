97dc6423ba255ac876c703c0ea55f8a27855aebb
Kasey <kasey.murdoch@yahoo.com>
2022-03-15 18:30:54 -0700

    Removed unused code

--- javascript.js
+++ javascript.js
@@ -15,9 +15,6 @@ function Book(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
-    // this.info = function() {
-    //     return title, author, pages, read
-    // }
 }
 
 function addBookToLibrary() {
